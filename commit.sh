d28b1111ef374260d7405e068a94693b4b4522f6
Yoshomay <isaiah.m.lange@gmail.com>
2025-05-27 03:18:39 -0700

    cant be bothered to enter what

--- commit.sh
+++ commit.sh
@@ -1,25 +1,5 @@
-#!/bin/bash
-# adds the changed files
-read -p "what files?: " updatedFiles
-if [ -z "$updatedFiles" ]; then
-    git add *
-else
-    git add $updatedFiles
-fi
-
-# ask what
-read -p "what changed?: " commitChanges
-
-# says whats changed
-if [ -z "$commitChanges" ]; then
-    git commit -m "cant be bothered to enter what"
-else
-    git commit -m "$commitChanges"
-fi
-
-# pushes them to the repo
-git push --force
-
-echo
-echo its updated
-read -p "Press enter to continue"
+git add *
+git commit -m "can't be bothered to enter what"
+git fetch origin
+git reset --hard origin/main
+git push -f
